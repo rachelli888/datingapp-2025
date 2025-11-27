2caac07a320577d99140f48b1c914a0d96d107b9
rachel <rachel.li@it.cn>
2025-11-27 15:58:19 +0800

    End of section 6

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,25 +1,23 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { lastValueFrom, single } from 'rxjs';
-import { errorContext } from 'rxjs/internal/util/errorContext';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/service/account-service';
-import { Home } from "../features/home/home";
-import { User } from '../types/user';
+//import { AccountService } from '../core/service/account-service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-  private accountService=inject(AccountService);
-  private http=inject(HttpClient); //构造函数注入
-  protected readonly title = "Dating app";  //signal('client');
-  //protected members:any; //当我们使用any类型时，实际上等于为这个属性关闭了TypeScript,
-  protected members = signal<User[]>([])
+export class App {
+  //private accountService=inject(AccountService);
+  protected router=inject(Router);
+
+  // private http=inject(HttpClient); //构造函数注入
+  // protected readonly title = "Dating app";  //signal('client');
+  // //protected members:any; //当我们使用any类型时，实际上等于为这个属性关闭了TypeScript,
+  // protected members = signal<User[]>([])
 
   //  ngOnInit(): void {
   //   this.http.get('https://localhost:5001/api/members').subscribe({
@@ -30,29 +28,29 @@ export class App implements OnInit {
   //   })
   // }
 
-  async ngOnInit() {
-    this.members.set(await this.getMembers());
-    this.setCurrentUser();
-  }
+  // async ngOnInit() {
+  //   //this.members.set(await this.getMembers());
+  //   // this.setCurrentUser();
+  // }
 
-  setCurrentUser()
-  {
-    const userString = localStorage.getItem('user');
-    if(!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
+  // setCurrentUser()
+  // {
+  //   const userString = localStorage.getItem('user');
+  //   if(!userString) return;
+  //   const user = JSON.parse(userString);
+  //   //this.accountService.currentUser.set(user);
+  // }
 
 
- async getMembers()
-  {
-    try {
-      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }    
-  }
+//  async getMembers()
+//   {
+//     try {
+//       return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
+//     } catch (error) {
+//       console.log(error);
+//       throw error;
+//     }    
+//   }
   
 
 }
--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,33 +1,40 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/service/account-service';
+import {Router, RouterLink, RouterLinkActive } from "@angular/router";
+import { ToastService } from '../../core/service/toast-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
 export class Nav {
   protected accountService= inject(AccountService);
+  private router=inject(Router);
+  private toast=inject(ToastService);
   protected creds: any={}
+  
   //protected loggedIn=signal(false)
 
   login()
   {
     this.accountService.login(this.creds).subscribe({
-      next: result =>{
-        // console.log(result);
-        // this.loggedIn.set(true);
+      next: () =>{
+        this.router.navigateByUrl('/members');
+        this.toast.success('Logged in successfully');
         this.creds={};
       }, 
-      error:error => alert(error.message)
+      error:error => {
+        this.toast.error(error.error);
+      }
     });
   }
 
   logout()
   {
-    //this.loggedIn.set(false);
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
